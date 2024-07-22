0e19d51155929c607182411b1dedc5c5840a01ff
SamJouini <samantha.u.h.jouini@gmail.com>
2024-07-22 14:54:58 +0200

    task 2

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,7 @@
+export default function getListStudentIds(array) {
+    if (Array.isArray(array)) {
+      const idList = array.map((student) => student.id);
+      return idList;
+    }
+    return [];
+  }
