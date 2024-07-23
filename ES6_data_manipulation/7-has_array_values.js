3012fe86365daac023f11ee6851695aad8797e8d
SamJouini <samantha.u.h.jouini@gmail.com>
2024-07-23 14:35:54 +0200

    test for error

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,4 +1,12 @@
 /* eslint-disable */
-function hasValuesFromArray(set, array) {
-    return array.every(element => set.has(element));
-  }  
+export default function createInt8TypedArray(length, position, value) {
+    const arrayBuffer = new ArrayBuffer(length);
+    const dataView = new DataView(arrayBuffer);
+  
+    if (position >= length || position < 0) {
+      throw new Error('Position outside range');
+    }
+  
+    dataView.setInt8(position, value); 
+    return dataView;
+  }
