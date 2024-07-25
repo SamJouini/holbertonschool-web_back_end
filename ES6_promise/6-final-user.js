105d67326b575399c15e286d3794a459f6864b3e
SamJouini <samantha.u.h.jouini@gmail.com>
2024-07-25 14:02:09 +0200

    ternary

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,6 +2,8 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.any([uploadPhoto(filename), signUpUser(firstName, lastName)]);
-}
+export default (firstName, lastName, fileName) => Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  .then((results) => results.map((promise) => ({
+    status: promise.status,
+    value: promise.status === 'fulfilled' ? promise.value : `${promise.reason.name}: ${promise.reason.message}`,
+  })));
