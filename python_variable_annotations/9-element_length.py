10aef0c1761bb14bb7bcd4d4bd0e5673dbfe91d2
SamJouini <samantha.u.h.jouini@gmail.com>
2024-08-20 13:26:26 +0200

    pyco

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -6,7 +6,7 @@ from typing import Iterable, List, Tuple, Sequence
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """    Create a list of tuples where each tuple contains a string from the input
-    and its length.
+    """Create a list of tuples where each tuple contains
+    a string from the input and its length.
     Returns a list of tuples, each containing a string and its length."""
     return [(i, len(i)) for i in lst]
