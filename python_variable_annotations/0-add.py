a4a20582a591060cd5ce7ca24993a5cb10bb04db
SamJouini <samantha.u.h.jouini@gmail.com>
2024-08-20 09:44:36 +0200

    pyco

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,2 +1,10 @@
 def add(a: float, b: float) -> float:
+    """
+    Add two floating-point numbers and return their sum.
+
+    This function takes two float arguments and returns their sum as a float.
+    If integer arguments are provided,
+    they will be implicitly converted to floats.
+
+    """
     return a + b
