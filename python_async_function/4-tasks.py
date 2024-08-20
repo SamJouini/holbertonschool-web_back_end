bf3c54e4609abbfdf027fd86928099533da9c285
SamJouini <samantha.u.h.jouini@gmail.com>
2024-08-20 16:53:56 +0200

    task

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -2,17 +2,15 @@
 " Create and return an asyncio.Task for wait_random."
 
 import asyncio
-from asyncio import Task
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Task:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Create and return an asyncio.Task for wait_random.
     Returns a task object for the wait_random coroutine."""
 
-    # Create a new event loop
-    loop = asyncio.get_event_loop()
-
-    # Create and return a Task for wait_random
-    return loop.create_task(wait_random(max_delay))
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
