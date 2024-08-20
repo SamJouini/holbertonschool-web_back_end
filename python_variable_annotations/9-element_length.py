7b29e9ad239bffbf69d67baf725f05ad1ee4ca09
SamJouini <samantha.u.h.jouini@gmail.com>
2024-08-20 13:25:23 +0200

    sequence type

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,10 +2,10 @@
 """Create a list of tuples where each tuple contains
 a string from the input and its length."""
 
-from typing import Iterable, List, Tuple
+from typing import Iterable, List, Tuple, Sequence
 
 
-def element_length(lst: Iterable[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """    Create a list of tuples where each tuple contains a string from the input
     and its length.
     Returns a list of tuples, each containing a string and its length."""
