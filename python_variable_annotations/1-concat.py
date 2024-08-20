dc1dc9d19246f2920225b4dccb659e107573a00d
SamJouini <samantha.u.h.jouini@gmail.com>
2024-08-20 11:53:36 +0200

    concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"Concatenate two strings and return the result."
+
+
+def concat(str1: str, str2: str) -> str:
+    """This function takes two string arguments and returns a new string
+    that is the concatenation of the input strings. The original strings
+    are not modified."""
+    return str1 + str2
