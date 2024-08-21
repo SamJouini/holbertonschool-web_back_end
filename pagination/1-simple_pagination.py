ec7754cd06fb9151fb8682ac6e8ebe03f057da12
SamJouini <samantha.u.h.jouini@gmail.com>
2024-08-21 15:57:19 +0200

    rename and organise

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -2,14 +2,13 @@
 """Calculate the start and end indexes for pagination."""
 
 import csv
+import math
 from typing import List, Tuple
 
 
 def index_range(page: int, page_size: int) -> tuple:
     """Calculate the start and end indexes for pagination.
     Returns a tuple containing the start index and end index."""
-    if page <= 0 or page_size <= 0:
-        return (0, 0)
 
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
