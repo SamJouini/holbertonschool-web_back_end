205c281dbe858087a3391a7e2e83fd96466c972e
SamJouini <samantha.u.h.jouini@gmail.com>
2024-08-23 17:08:33 +0200

    insert school

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a document into the MongoDB collection and returns the inserted document's ID.
+    """
+    document = kwargs
+    inserted_doc = mongo_collection.insert_one(document)
+    inserted_doc_id = inserted_doc.inserted_id
+    return inserted_doc_id
