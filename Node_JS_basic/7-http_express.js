e51e46496cb2d2d0ed784c5ae62c54e6caa868a1
SamJouini <samantha.u.h.jouini@gmail.com>
2024-09-19 15:25:16 +0200

    new approch

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,31 +1,28 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
 
-const app = express();
-const port = 1245;
+const countStudents = require('./3-read_file_async');
 const database = process.argv[2];
 
+const app = express();
+
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
-});
-app.get('/students', (req, res) => {
-  countStudents(database)
-    .then((data) => {
-      const { students, csStudents, sweStudents } = data;
-      res.write('This is the list of our students\n');
-      res.write(`Number of students: ${students.length}\n`);
-      res.write(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}\n`);
-      res.write(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
-      res.statusCode = 200;
-      res.end();
-    })
-    .catch((error) => {
-      res.statusCode = 500;
-      res.send(`This is the list of our students\n${error.message}`);
-    });
+  res.type('text').send('Hello Holberton School!');
 });
 
+app.get('/students', async (req, res) => {
+  res.type('text');
+
+  try {
+    const output = await countStudents(database);
+    res.send(`This is the list of our students\n${output}`);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+const port = 1245;
 app.listen(port, () => {
-
+  console.log(`Server is listening on port ${port}`);
 });
+
 module.exports = app;
