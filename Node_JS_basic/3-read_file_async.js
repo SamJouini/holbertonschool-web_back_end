6759bd3570a7c958e6e2df59144c388e342af85b
SamJouini <samantha.u.h.jouini@gmail.com>
2024-09-19 14:48:09 +0200

    revert changes

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,56 +1,56 @@
 const fs = require('fs').promises;
 
 async function countStudents(path) {
-	try {
-		const data = await fs.readFile(path, 'utf8');
-		const lines = data.trim().split('\n');
+  try {
+    const data = await fs.readFile(path, 'utf8');
+    const lines = data.trim().split('\n');
 
-		if (lines.length <= 1) {
-			throw new Error('No students found in the database');
-		}
+    if (lines.length <= 1) {
+      throw new Error('No students found in the database');
+    }
 
-		const fields = {};
-		const students = [];
-		lines.shift();
+    const fields = {};
+    const students = [];
+    lines.shift();
 
-		for (const line of lines) {
-			const trimmedLine = line.trim();
+    for (const line of lines) {
+      const trimmedLine = line.trim();
 
-			if (trimmedLine !== '') {
-				const student = trimmedLine.split(',');
+      if (trimmedLine !== '') {
+        const student = trimmedLine.split(',');
 
-				if (student.length >= 4) {
-					const firstName = student[0];
-					const field = student[3];
+        if (student.length >= 4) {
+          const firstName = student[0];
+          const field = student[3];
 
-					students.push(firstName);
+          students.push(firstName);
 
-					if (!fields[field]) {
-						fields[field] = {
-							count: 0,
-							names: [],
-						};
-					}
+          if (!fields[field]) {
+            fields[field] = {
+              count: 0,
+              names: [],
+            };
+          }
 
-					fields[field].count += 1;
-					fields[field].names.push(firstName);
-				}
-			}
-		}
+          fields[field].count += 1;
+          fields[field].names.push(firstName);
+        }
+      }
+    }
 
-		let output = `Number of students: ${students.length}\n`;
+    let output = `Number of students: ${students.length}\n`;
 
-		for (const field in fields) {
-			if (Object.hasOwn(fields, field)) {
-				const { count, names } = fields[field];
-				output += `Number of students in ${field}: ${count}. List: ${names.join(', ')}\n`;
-			}
-		}
+    for (const field in fields) {
+      if (Object.hasOwn(fields, field)) {
+        const { count, names } = fields[field];
+        output += `Number of students in ${field}: ${count}. List: ${names.join(', ')}\n`;
+      }
+    }
 
-		return output.trim();
-	} catch (err) {
-		throw new Error('Cannot load the database');
-	}
+    return output.trim();
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
