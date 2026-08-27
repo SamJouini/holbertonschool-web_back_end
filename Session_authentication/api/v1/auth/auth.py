4e3b42b47ff817261e48468f7bd42789cb1190cb
SamJouini <samantha.u.h.jouini@gmail.com>
2026-08-27 11:57:48 +0200

    Session authentication

---
+++ Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Auth module
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+User = TypeVar('User')
+
+
+class Auth:
+    """Auth class"""
+
+    def require_auth(self, path: str,
+                     excluded_paths: List[str]) -> bool:
+        """Returns True if authentication is required for the path."""
+        if path is None:
+            return True
+
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        if not path.endswith('/'):
+            path += '/'
+
+        return path not in excluded_paths
+
+    def authorization_header(self, request=None) -> str:
+        """Returns the value of the Authorization header."""
+        if request is None:
+            return None
+
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns the current user."""
+        return None
