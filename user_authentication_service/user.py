438694a1c85e0ad2548fd1b753a81682690ef58a
SamJouini <samantha.u.h.jouini@gmail.com>
2026-08-27 13:22:35 +0200

    User authentication

---
+++ user_authentication_service/user.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""User model module
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """Represents a user record in the database.
+
+    Attributes:
+        id (int): The primary key of the user.
+        email (str): The user's email address.
+        hashed_password (str): The user's hashed password.
+        session_id (str): The user's current session ID.
+        reset_token (str): The user's password reset token.
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
